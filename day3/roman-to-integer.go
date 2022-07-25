00769a1a1e6ecc14b6f2092636d43614efdcd561
nguyenhy <nguyenhy311@gmail.com>
2022-07-25 21:10:30 +0700

    feat(leet): resolve roman to integer

--- day3/roman-to-integer.go
+++ day3/roman-to-integer.go
@@ -9,27 +9,59 @@ import (
 )
 
 func main() {
-	fmt.Println(romanToInt("III"))
+	test := []string{"III", "II", "IV", "V", "VI", "VII", "VIII", "IX", "XII", "XIII", "XIV", "XIX", "MCMXCIV", "XC"}
+	for i := 0; i < len(test); i++ {
+		fmt.Println(test[i], romanToInt(test[i]))
+		fmt.Println("----------------------")
+	}
 }
 
 func romanToInt(s string) int {
-	length := len(s)
 
+	// create roman to integer map
 	romanCharMap := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-	fmt.Println(romanCharMap)
+	// check if `s` in `romanCharMap`, for better perf
+	if romanCharMap[s] > 0 {
+		return romanCharMap[s]
+	}
+
+	length := len(s)
 	var output int = 0
+
+	// loop from right to left
 	for i := length - 1; i >= 0; i-- {
+		// roman number at `i` position
 		romanNumber := string(s[i])
+
+		// corresponding integer value of `romanNumber`
 		intNumber := romanCharMap[romanNumber]
 
-		fmt.Println(i, romanNumber, intNumber, output)
-		if intNumber >= 5 {
+		// only increment if new value is at least `5x` current sum (output)
+		if 5*intNumber > output {
+			/*
+				Eg: XC ( 90 )
+				index 		= 1
+				intNumber 	= 100
+				output	 	= 0
+				--------
+				5 * 100 > 0 : true
+				------
+				output 		= 0 + 100
+
+
+				index 		= 0
+				intNumber 	= 10
+				output	 	= 100
+				--------
+				5 * 10 > 100: false
+				------
+				output 		= 100 - 10 = 90
+			*/
 			output += intNumber
 		} else {
 			output -= intNumber
 		}
-		fmt.Println("->", output)
 	}
 
 	return output
