e89d1de5e7dfb3fa6824b4795296b250ebcea175
nguyenhy <nguyenhy311@gmail.com>
2022-10-10 14:24:16 +0700

    feat: move leet to go

---
+++ go/21-merge-two-sorted-lists.go
@@ -0,0 +1,17 @@
+/*
+ - problem: https://leetcode.com/problems/merge-two-sorted-lists/
+ - solution:
+*/
+package main
+
+func main() {
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	//
+}
