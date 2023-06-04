d22dd6c3487a661f46446bfeae0eca77df4e6434
nguyenhy <nguyenhy311@gmail.com>
2023-06-04 09:23:01 +0700

    feat(go.leet.21): follow the guide

--- golang/leet/21-merge-two-sorted-lists/main.go
+++ golang/leet/21-merge-two-sorted-lists/main.go
@@ -35,5 +35,41 @@ func (node *ListNode) ToString() string {
 	return result
 }
 
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) /* *ListNode */ {
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	var pointer *ListNode = list1
+	if list1.Val > list2.Val {
+		pointer = list2
+		list2 = list2.Next
+	} else {
+		list1 = list1.Next
+	}
+
+	var current *ListNode = pointer
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+
+		current = current.Next
+	}
+
+	if list1 == nil {
+		current.Next = list2
+	} else if list2 == nil {
+		current.Next = list1
+	}
+
+	return pointer
 }
