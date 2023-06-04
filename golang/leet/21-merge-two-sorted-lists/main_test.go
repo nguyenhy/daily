a45579d822fafb53716db86a2ad5230969f653d6
nguyenhy <nguyenhy311@gmail.com>
2023-06-04 09:23:12 +0700

    feat(do.leet.21): create test case

--- golang/leet/21-merge-two-sorted-lists/main_test.go
+++ golang/leet/21-merge-two-sorted-lists/main_test.go
@@ -2,7 +2,93 @@ package mergetwosortedlists
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test(t *testing.T) {
+	assert.Equal(t, (&ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3}}}).ToString(), "[1,2,3]")
+
+	assert.Equal(
+		t,
+		"[0]",
+		mergeTwoLists(nil, &ListNode{}).ToString(),
+	)
+	assert.Equal(
+		t,
+		"[0]",
+		mergeTwoLists(&ListNode{Val: 0}, nil).ToString(),
+	)
+	assert.Equal(
+		t,
+		"[0]",
+		mergeTwoLists(&ListNode{Val: 0}, nil).ToString(),
+	)
+	assert.Equal(
+		t,
+		"[0]",
+		mergeTwoLists(&ListNode{Val: 0}, nil).ToString(),
+	)
+	assert.Equal(
+		t,
+		"[1,1,2,2,3,3]",
+		mergeTwoLists(
+			&ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 3,
+					},
+				},
+			},
+			&ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 3,
+					},
+				},
+			},
+		).ToString(),
+	)
+
+	assert.Equal(
+		t,
+		"[1,1,2,3,4,4]",
+		mergeTwoLists(
+			&ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 4,
+					},
+				},
+			},
+			&ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 3,
+					Next: &ListNode{
+						Val: 4,
+					},
+				},
+			},
+		).ToString(),
+	)
+
+	assert.Equal(
+		t,
+		"[1,2]",
+		mergeTwoLists(
+			&ListNode{
+				Val: 2,
+			},
+			&ListNode{
+				Val: 1,
+			},
+		).ToString(),
+	)
 }
