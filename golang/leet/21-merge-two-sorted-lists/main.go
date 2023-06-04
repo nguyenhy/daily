4945825ccc2d7e5e9f219fcc5f5d8fc462f8045c
nguyenhy <nguyenhy311@gmail.com>
2023-06-04 09:22:21 +0700

    feat(go.leet.21): add method `ToString` to `ListNode`

--- golang/leet/21-merge-two-sorted-lists/main.go
+++ golang/leet/21-merge-two-sorted-lists/main.go
@@ -4,9 +4,35 @@
 */
 package mergetwosortedlists
 
+import (
+	"strconv"
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func (node *ListNode) ToString() string {
+	if node == nil {
+		return "[]"
+	}
+
+	result := "["
+
+	for node != nil {
+		result += strconv.Itoa(node.Val)
+
+		if node.Next != nil {
+			result += ","
+		}
+
+		node = node.Next
+	}
+
+	result += "]"
+
+	return result
 }
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) /* *ListNode */ {
